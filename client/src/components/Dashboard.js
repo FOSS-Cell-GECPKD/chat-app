dc8ba0d1a3b20a25cfc71ae96f37117c64e9a7c5
VipinVIP <kpvipin33@gmail.com>
2020-10-01 18:19:09 +0530

    Added contacts saving

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,6 +3,8 @@ import Sidebar from './Sidebar'
 
 export default function Dashboard({id}) {
     return (
-       <Sidebar id={id}/>
+        <div className= "d-flex" style={{height:'100vh'}}>
+        <Sidebar id={id}/>
+        </div>
     )
 }
--- client/src/components/NewConversationModel.js
+++ client/src/components/NewConversationModel.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Conversations() {
+export default function NewConversationModel() {
     return (
         <div>
-            Conversations
+            
         </div>
     )
 }
