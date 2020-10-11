cc438bd46f76d4f3519137a2d19dd69036a60ce6
VipinVIP <kpvipin33@gmail.com>
2020-10-11 11:02:29 +0530

    Added  messages view

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
+import React, {  useState , useCallback} from 'react'
 import {Button, Form, FormGroup, InputGroup} from 'react-bootstrap' 
 import { useConversations } from '../contexts/ConversationsProvider'
 
 export default function OpenConversation() {
     const [text,setText]=useState('')
+    const setRef=useCallback(node=>{
+        if (node){node.scrollIntoView({smooth:true})}
+        
+    },[])
+
     const {sendMessage,selectedConversation}=useConversations()
     function handleSubmit(e){
         e.preventDefault()
         sendMessage(selectedConversation.recipients.map(r=>r.id),text)
         setText('')
     }
+
+    
+
     return (
         <div class="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
+                <div className="d-flex flex-column align-items-start justify-content-end px-3">
 
+                    {selectedConversation.messages.map((message,index)=>
+                    { 
+                        const lastMessage=selectedConversation.messages.length-1 ===index
+                        return(
+                            <div 
+                            ref={lastMessage?setRef:null}
+                            key={index}
+                            className={`my-1 d-flex flex-column ${message.fromMe?`align-self-end`:''}`}>
+                                <div
+                                 className={`rounded px-2 py-1 ${message.fromMe?`bg-primary text-white`:`border`}`}>
+                                    {message.text}
+                                </div>
+                                <div className={`text-muted small ${message.fromMe?`text-right`:``} `}>
+                                    {message.fromMe?'You':message.senderName}
+                                </div>
+                                
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
             <Form onSubmit={handleSubmit}>
                 <FormGroup className="m-2">
--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -22,6 +22,26 @@ export  function ConversationsProvider({id,children}) {
 
     function addMessageToConversation({recipients,text,sender}){
 
+        setConversations(prevConversations=>{
+            let madeChange = false
+            const newMessage={sender,text }
+            const newConversations=prevConversations.map(conversation=>{
+                if (arrayEquality(conversation.recipients,recipients)){
+                    madeChange=true
+                    return {...conversation,
+                        messages:[...conversation.messages,newMessage]}
+                }
+
+                return conversation
+            })
+
+            if (madeChange){
+                return newConversations
+            }else{
+                return[...prevConversations,{recipients,message:[newMessage]}]
+            }
+        })
+
     }
     function sendMessage(recipients,text){
         addMessageToConversation({recipients,text,sender:id})
@@ -34,8 +54,21 @@ export  function ConversationsProvider({id,children}) {
             const name= (contact && contact.name) || recipient
             return {id:recipient,name}
         })
+
+        const messages= conversation.messages.map(message=>{
+
+            const contact=contacts.find(contact =>{
+                return contact.id===message.sender
+            })
+            const name= (contact && contact.name) || message.sender
+            const fromMe=id===message.sender
+
+            return {...message,senderName:name ,fromMe}
+
+        })
+
         const selected=index===selectConversationIndex
-        return {...conversation,recipients,selected}
+        return {...conversation,messages,recipients,selected}
     })
 
     const value={
@@ -53,3 +86,12 @@ export  function ConversationsProvider({id,children}) {
     )
 }
 
+function arrayEquality(a,b){
+    if(a.length!==b.length) return false
+
+    a.sort()
+    b.sort()
+    return a.every((element,index)=>{
+        return element ===b[index]
+    })
+}
